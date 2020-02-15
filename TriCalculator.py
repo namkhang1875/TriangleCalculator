be1556341eb9cc7c36050cba1528086828f4f83a
IfrongS <31177273+IfrongS@users.noreply.github.com>
2020-02-16 04:49:29 +0700

    Update TriCalculator.py

--- TriCalculator.py
+++ TriCalculator.py
@@ -1,3 +1,21 @@
 class TriCalculator:
     def cal(self, sideA, sideB, sideC):
-        pass
+    	a = pow(sideA,2)
+    	b = pow(sideB,2)
+    	c = pow(sideC,2)
+    	if sideA == sideB and sideA == sideC:
+    		print("equilateral triangle")
+    	elif sideA == sideB or sideA == sideC or sideB == sideC:
+    		print("isosceles triangle")
+    	else: 
+    		if a > b and a > c and a == b + c: #a ค่ามากสุด
+    			print("right triangle sideA is the most")
+    		elif b > a and b > c and b == a + c: #b ค่ามากสุด
+    			print("right triangle sideB is the most")
+    		elif c > a and c > b and c == a + b: #c ค่ามากสุด
+    			print("right triangle sideC is the most")
+    		else:
+    			print("scalene")
+
+t = TriCalculator()
+t.cal(5,4,3)
