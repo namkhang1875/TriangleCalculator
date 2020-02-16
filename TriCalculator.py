de0f1f1d98b9d4cc133f4d4877d85c78262c1b35
IfrongS <31177273+IfrongS@users.noreply.github.com>
2020-02-16 18:03:43 +0700

    Update TriCalculator.py

--- TriCalculator.py
+++ TriCalculator.py
@@ -1,21 +1,35 @@
 class TriCalculator:
     def cal(self, sideA, sideB, sideC):
-    	a = pow(sideA,2)
-    	b = pow(sideB,2)
-    	c = pow(sideC,2)
-    	if sideA == sideB and sideA == sideC:
-    		print("equilateral triangle")
-    	elif sideA == sideB or sideA == sideC or sideB == sideC:
-    		print("isosceles triangle")
-    	else: 
-    		if a > b and a > c and a == b + c: #a ค่ามากสุด
-    			print("right triangle sideA is the most")
-    		elif b > a and b > c and b == a + c: #b ค่ามากสุด
-    			print("right triangle sideB is the most")
-    		elif c > a and c > b and c == a + b: #c ค่ามากสุด
-    			print("right triangle sideC is the most")
-    		else:
-    			print("scalene")
+    	#some input is 0
+    	if sideA == 0:
+    		print("error sideA")
+    	elif sideB == 0:
+    		print("error sideB")
+    	elif sideC == 0:
+    		print("error sideC")
+    	else:
+    		a = pow(sideA,2)
+    		b = pow(sideB,2)
+    		c = pow(sideC,2)
+    		#all input is equal
+    		if sideA == sideB and sideA == sideC:
+    			print("equilateral triangle")
+    		#2-input is equal
+    		elif sideA == sideB or sideA == sideC or sideB == sideC:
+    			print("isosceles triangle")
+    		else: 
+    			#a is the most
+    			if a > b and a > c and a == b + c: 
+    				print("right triangle")
+    			#b is the most
+    			elif b > a and b > c and b == a + c: 
+    				print("right triangle")
+    			#c is the most
+    			elif c > a and c > b and c == a + b: 
+    				print("right triangle")
+    			#all input are not equal
+    			else:
+    				print("scalene triangle")
 
-t = TriCalculator()
-t.cal(5,4,3)
+#t = TriCalculator()
+#t.cal(5,4,3)
