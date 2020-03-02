c2f03b4ec5689730a5f18dd973734b8ce16486ef
namkhang1875 <ice9966@hotmail.com>
2020-03-03 01:30:28 +0700

    All bug fixed !!! let's sleep ~
    -fix the undefined sideA B C
    -fix the circular import issue (Big issue)

--- EnterButton.py
+++ EnterButton.py
@@ -1,18 +1,11 @@
-# import ResultDisplay
 class EnterButton:
-    
-    def __init__(self):
-        
-
     def onClick(self,sideA, sideB, sideC):
         from ResultDisplay import ResultDisplay
-        triType = ""
-        # from TriCalculator import TriCalculator     
-        # triCal = TriCalculator()
-        # resultDisplay = ResultDisplay()
-        # triType = triCal.cal(sideA, sideB, sideC)
+        from TriCalculator import TriCalculator     
+        triCal = TriCalculator()
+        resultDisplay = ResultDisplay()
+        triType = triCal.cal(sideA, sideB, sideC)
         if triType is not None:
-            pass
-            # resultDisplay.drawTri(sideA, sideB, sideC, triType)
+            resultDisplay.drawTri(sideA, sideB, sideC, triType)
         else:
             print(triType)
