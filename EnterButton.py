2260f09a22bf20d1848510adc9b43969646c4f67
namkhang1875 <ice9966@hotmail.com>
2020-03-08 15:15:02 +0700

    All feature done ! (but have not test yet)

--- EnterButton.py
+++ EnterButton.py
@@ -4,14 +4,23 @@ from tkinter import *
 import ResultDisplay
 import TriCalculator
 
+
 class EnterButton:
-    def onClick(self,sideA, sideB, sideC):
+    triType = ""
+
+    def onClick(self, sideA, sideB, sideC):
         from ResultDisplay import ResultDisplay
-        from TriCalculator import TriCalculator     
+        from TriCalculator import TriCalculator
+        from UserInterface import UserInterface
         triCal = TriCalculator()
         resultDisplay = ResultDisplay()
-        triType = triCal.cal(sideA, sideB, sideC)
-        if triType is not None:
-            resultDisplay.drawTri(sideA, sideB, sideC, triType)
+
+        self.triType = triCal.cal(sideA, sideB, sideC)
+
+        if self.triType is not None:
+            resultDisplay.drawTri(sideA, sideB, sideC, self.triType)
         else:
-            print(triType)
+            print(self.triType)
+        side1Text = tk.Label(UserInterface.root,
+                             text=self.triType, font="Verdana 14")
+        side1Text.place(x=675, y=450)
